099402aa68a1b3ea41ac2e72c916f576b0c26d37
Sai Boorlagadda <sboorlagadda@pivotal.io>
2018-08-06 15:45:36 -0700

    running LocatorLauncherRemoteIntegrationTest

--- ci/scripts/test-run-flaky.sh
+++ ci/scripts/test-run-flaky.sh
@@ -143,7 +143,7 @@ pushd ${GEODE_BUILD}
   echo "Running tests"
   set -x
 
-  ./gradlew --no-daemon -x javadoc -x spotlessCheck :geode-core:integrationTest --tests org.apache.geode.distributed.ServerLauncherRemoteWithCustomLoggingIntegrationTest
+  ./gradlew --no-daemon -x javadoc -x spotlessCheck :geode-core:integrationTest --tests org.apache.geode.distributed.LocatorLauncherRemoteIntegrationTest
   export GRADLE_EXIT_STATUS=$?
   set +x
 popd
